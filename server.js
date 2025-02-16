8283ef0c45ddfb1946f062a6e6d581517167a105
Umair Dada <75429136+umairdadaa@users.noreply.github.com>
2025-02-15 21:25:52 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 // server.js
 
 import startServer from "./index.js";
+import http from "http";  // Import the built-in http module
 
-export default {
-  async fetch(request, env) {
-    // Call startServer() here or wherever appropriate
+const requestListener = (req, res) => {
+  const url = new URL(req.url, `http://${req.headers.host}`);
+
+  if (url.pathname === "/") {
     startServer();
-
-    // Respond to the fetch request
-    return new Response("Hello world");
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello world");
+  } else if (url.pathname === "/another-endpoint") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("This is another endpoint");
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Not found");
   }
 };
+
+const server = http.createServer(requestListener); // Create the server
+
+server.listen(3000, () => {
+  console.log("Server is listening on port 3000");
+});
